136b223deb79ce961049c7c3777cfc00c921164e
Robert Rodriguez <robert@robertrodriguez.io>
2023-09-19 22:09:37 -0700

    WIP: Homepage animations

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 import { useRef } from "react"
 import * as styles from './index.module.scss'
-import { motion } from "framer-motion"
+import { motion, useScroll } from "framer-motion"
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import { StaticImage } from "gatsby-plugin-image"
@@ -19,10 +19,39 @@ import trainingSupport from '../images/training-support.svg';
 
 
 const IndexPage = (props) => {
-  const myRef = useRef(null)
 
+  const myRef = useRef(null)
   const executeScroll = () => myRef.current.scrollIntoView({behavior: 'smooth'});
 
+  const IntroTextP = {
+    hidden: {
+
+    },
+    visible: {
+      transition: {
+        delayChildren: 1.5,
+        when: "beforeChildren",
+        staggerChildren: .5,
+      }
+    }
+  }
+
+  const IntroTextVariants = {
+    hidden: {
+      opacity: 0,
+      x: -20,
+    },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: .75,
+        opacity: { duration: 1 }
+      }
+    }
+  }
+
+  const { scrollYProgress } = useScroll();
 
   return (
     <Layout>
@@ -30,22 +59,72 @@ const IndexPage = (props) => {
       <div className={styles.hero}>
         <div className={styles.heroContainer}>
 
-          <StaticImage src="../images/headshot-thumbnail.png" alt="" placeholder="blurred" quality={100} className={styles.headshotThumbnail} />
+          <motion.div
+          initial={{
+            y: -50,
+            opacity: 0,
+          }}
+          animate={{
+            y: 0,
+            opacity: 1,
+            transition: {
+              delay: .25,
+              duration: .75,
+              opacity: { duration: 1 }
+            }
+          }}>
+            <StaticImage src="../images/headshot-thumbnail.png" alt="" placeholder="blurred" quality={100} className={styles.headshotThumbnail} />
+          </motion.div>
 
           <div className={styles.heroTextContainer}>
 
-            <p className={styles.heroTextTitle}>Hello, I'm Robert, a web designer and developer.</p>
+            <motion.p
+            initial={{
+              opacity: 0,
+              y: 20
+            }}
+            animate={{
+              opacity: 1,
+              y: 0,
+              transition: {
+                delay: .5,
+                duration: 1
+              }
+            }}
+            className={styles.heroTextTitle}>
+              Hello, I'm Robert, a web designer and developer.
+            </motion.p>
             
-            <p className={styles.heroTextIntro}>
-              I craft <span className={styles.unique}>unique</span> web experiences.
-            </p>
+            <motion.p
+            className={styles.heroTextIntro}
+            initial="hidden"
+            animate="visible"
+            variants={IntroTextP}>
+              <motion.span className="display-inline-block" variants={IntroTextVariants}>I craft&nbsp;</motion.span>
+              <Unique variants={IntroTextVariants} />
+              <motion.span className="display-inline-block" variants={IntroTextVariants}>&nbsp;web experiences.</motion.span>
+            </motion.p>
             
 
             {/* <a href="#" className="button primary">Let's Work Together <ChevronRight size={18} /></a>
             &nbsp;&nbsp;&nbsp;
             <a href="#" className="button default outline">About Me <ChevronRight size={18} /></a> */}
 
-            <div className={styles.heroCTAContainer}>
+            <motion.div className={styles.heroCTAContainer}
+            initial={{
+              opacity: 0,
+              y: -50
+            }}
+            animate={{
+              opacity: 1,
+              y: 0
+            }}
+            transition={{
+              delay: 3,
+              duration: .75,
+              opacity: { duration: 1 }
+            }}
+            >
               <motion.a
                 initial={{
                   scale: 1
@@ -83,9 +162,9 @@ const IndexPage = (props) => {
                 }}
                 className="button default outline"
               >
-                Explore My Work <ChevronDown size={18} />
+                View Recent Projects <ChevronDown size={18} />
               </motion.button>
-            </div>
+            </motion.div>
 
           </div>
         </div>
@@ -95,18 +174,24 @@ const IndexPage = (props) => {
           onClick={executeScroll}
           initial={{
             x: '-50%',
+            opacity: 0
           }}
           animate={{
+            opacity: 1,
             y: 25,
             x: '-50%'
           }}
           transition={{
-            ease: 'easeInOut',
-            duration: 0.7,
-            delay: 1,
-            repeat: Infinity,
-            repeatType: 'mirror',
-            repeatDelay: 0,
+            y: {
+              repeat: Infinity,
+              repeatType: 'mirror',
+              repeatDelay: 0,
+              duration: 0.7,
+              ease: 'easeInOut',
+            },
+            opacity: {
+              delay: 3.25
+            }
           }}
         >
           <ChevronsDown size={22} />
@@ -117,13 +202,20 @@ const IndexPage = (props) => {
           <path className={styles.backgroundLogoPath} d="m367.6,236.91c-5.66,3.29-12.13,5.16-19.17,5.17h-48.4l17.8,21.24,104.33,121.43,20.8,24.21c35.64-43.2,57.04-98.58,57.04-158.96C500,111.93,388.07,0,250,0,181.99,0,120.34,27.17,75.27,71.22l20.5,23.89c9.79-9.78,20.49-18.65,32-26.42,34.91-23.58,76.89-37.33,122.23-37.33,30.23,0,58.95,6.12,85.1,17.17,39.22,16.58,72.63,44.33,96.21,79.23,23.58,34.92,37.33,76.89,37.33,122.23,0,30.23-6.12,58.95-17.17,85.1-3.17,7.51-6.76,14.79-10.73,21.85l-72.7-84.92c5.67-1.64,11.06-3.95,16.03-6.85,10.54-6.14,19.31-14.89,25.5-25.4,6.18-10.5,9.74-22.82,9.73-35.84,0-13.02-3.55-25.34-9.73-35.84-6.18-10.51-14.96-19.26-25.49-25.4-10.44-6.1-22.69-9.63-35.64-9.62h-219l170.65,201.17h-148.51c-7.04,0-13.51-1.88-19.17-5.17-5.67-3.31-10.45-8.06-13.77-13.72-3.32-5.66-5.22-12.18-5.22-19.27,0-7.09,1.9-13.61,5.22-19.27,3.32-5.66,8.09-10.41,13.77-13.71,5.66-3.29,12.13-5.17,19.17-5.17h48.4l-17.8-21.24-104.33-121.43-20.8-24.21C21.41,134.24,0,189.62,0,250c0,138.07,111.93,250,250,250,67.83,0,129.33-27.03,174.37-70.89l-20.48-23.86c-9.7,9.63-20.29,18.38-31.66,26.06-34.91,23.58-76.89,37.33-122.23,37.33-30.23,0-58.95-6.12-85.1-17.17-39.22-16.58-72.63-44.33-96.21-79.23-23.58-34.92-37.33-76.89-37.33-122.23,0-30.23,6.12-58.95,17.17-85.1,3.17-7.5,6.76-14.79,10.73-21.85l72.7,84.92c-5.67,1.64-11.06,3.95-16.03,6.85-10.53,6.14-19.32,14.89-25.49,25.4-6.18,10.5-9.74,22.82-9.73,35.84,0,13.02,3.55,25.34,9.73,35.84,6.18,10.51,14.96,19.26,25.49,25.4,10.45,6.1,22.69,9.63,35.64,9.62h219l-170.65-201.17h148.51c7.04,0,13.51,1.88,19.17,5.17,5.67,3.3,10.45,8.06,13.77,13.71,3.32,5.67,5.22,12.18,5.22,19.27,0,7.09-1.9,13.61-5.22,19.27-3.32,5.66-8.09,10.41-13.77,13.72Z"/>
         </svg>
       </div>
-      <div className="container small">
-        <p>Dolor aute veniam dolor est ullamco amet id voluptate proident duis. Cillum mollit ex ex ullamco dolor ipsum laboris Lorem culpa. Minim laboris excepteur duis sint. Quis tempor reprehenderit velit nostrud duis veniam in dolore eiusmod cupidatat pariatur. Ad cupidatat reprehenderit Lorem anim commodo. Sint ipsum ipsum aliqua culpa nulla nisi non.</p>
-        <p>Exercitation elit voluptate magna officia nulla nisi quis labore magna exercitation. Aliquip laborum cillum magna sunt nostrud. Minim consectetur anim esse labore voluptate irure adipisicing do do irure do ea tempor. Pariatur fugiat sit velit consequat consectetur fugiat labore voluptate. Ipsum exercitation aliqua aute amet dolore culpa magna dolor ex cupidatat ut ullamco anim. Non aute ut nisi ad anim est ullamco pariatur non duis.</p>
-        <p>Reprehenderit elit nisi esse commodo adipisicing excepteur anim exercitation. Ea ea culpa laboris dolor quis consectetur commodo esse laborum tempor. Mollit duis ad minim ut proident in quis velit aliqua. Consequat laborum excepteur nisi consequat dolor laborum.</p>
-        <div ref={myRef}>test</div>
-        <p>Exercitation laborum non dolore quis. Velit do officia laboris labore minim. Sunt aliqua minim velit laboris elit qui ipsum labore anim in eu esse cupidatat.</p>
-        <p>Enim laboris tempor culpa ad commodo cillum excepteur occaecat qui aute aute non non duis. Magna irure aliquip anim aliqua in Lorem ut anim nulla ut veniam non. Excepteur eiusmod deserunt consequat adipisicing duis reprehenderit labore ea consectetur mollit duis minim commodo esse. Excepteur Lorem mollit occaecat nostrud cillum magna ipsum. Fugiat elit sint fugiat reprehenderit eiusmod cupidatat. Esse ipsum consequat id nulla excepteur qui.</p>
+      <div className={styles.recentProjects}>
+        <h2 className="text-center">Recent Projects</h2>
+
+        <div className={`${styles.project} container`}>
+          <div className={styles.projectText}>
+            <h3>Project Title</h3>
+            <p>Lorem Officia tempor commodo ex deserunt proident. Adipisicing ullamco velit qui sit et nostrud ea. Ipsum Lorem sunt id excepteur laboris. Sunt minim minim ullamco ad ad.</p>
+            <a href="#" className="button primary">View Project</a>
+          </div>
+          <motion.div style={{ y: scrollYProgress }}>
+            <StaticImage src="../images/laptop-image.png" alt="" placeholder="blurred" quality={80} className={styles.projectImage} />
+          </motion.div>
+        </div>
+
       </div>
       <div className={styles.services}>
         <div className="container">
