6224f0dcea393e8ce150d8bfd46ee7e204d0e21a
Robert Rodriguez <robert@robertrodriguez.io>
2025-07-31 09:00:20 -0700

    Update site metadata and add manifest plugin
    Enhanced site metadata in gatsby-config.js for improved SEO and branding, added gatsby-plugin-manifest and gatsby-plugin-google-gtag for PWA and analytics support, and updated dependencies accordingly. Also removed the JavaScript skill logo from the homepage and updated the SEO title to reflect Los Angeles location. Updated .gitignore to ignore all .env.* files.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,9 +3,9 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Robert Rodriguez - Website Design and Development`,
-    description: `default`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Robert Rodriguez - Website Design and Development in Los Angeles`,
+    description: `Robert Rodriguez is a self-taught web designer and developer with 15+ years of experience crafting responsive, accessible, and user-focused websites. From strategy to design, development, and support. He builds tailored digital solutions for businesses and organizations of all sizes.`,
+    siteUrl: `https://www.robertrodriguez.io`
   },
   plugins: [
     'gatsby-plugin-sass',
@@ -18,6 +18,26 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images/`,
       },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png",
+        name: `Robert Rodriguez - Website Design and Development in Los Angeles`,
+        short_name: `Robert Rodriguez`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#16181A`,
+        display: `standalone`,
+      }
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        trackingIds: [
+          "G-PNC393N016", // Google Analytics / GA
+        ],
+      }
     },
   ]
 };
